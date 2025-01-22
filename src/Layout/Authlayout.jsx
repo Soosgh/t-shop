a10346d3560aff146cd7d788f76d3be0b6d18803
Sura5 <surawg5@gmail.com>
2025-01-22 08:55:05 +0200

    Resolve merge conflicts

--- src/Layout/Authlayout.jsx
+++ src/Layout/Authlayout.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import navbarcustom from '../component/user/Navbar/Navbar';
 
 export default function Authlayout() {
     return (
-        < > <navbarcustom />'
-
-        </ >
+        <></>
     )
 }
