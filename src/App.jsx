b000c6fd4b240f51c6cbd6b77d1478bae1ccc347
Sura5 <surawg5@gmail.com>
2025-01-22 08:49:00 +0200

    Fix bootstrap import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarCustom from './component/user/Navbar/Navbarr';
+
 
 function App() {
-
-
   return (
-    <>
-
-
-    </>
-  )
+    <div>
+      <NavbarCustom />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Layout/Authlayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import navbarcustom from '../component/user/Navbar/Navbar';
+
+export default function Authlayout() {
+    return (
+        < > <navbarcustom />'
+
+        </ >
+    )
+}
